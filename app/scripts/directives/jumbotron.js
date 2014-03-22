1b8d3d88db5151d60fcb378a8ec1b9b653ad1c1f
nickeblewis <nickeblewis@gmail.com>
2014-03-22 16:57:56 +0000

    first commit

---
+++ app/scripts/directives/jumbotron.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('farnboroughyoApp')
+  .directive('jumbotron', function () {
+    return {
+			template: '<div class="jumbotron"><h1>{{jtHeading}}</h1><p class="lead"><img src="images/yeoman.png" alt="Im Yeoman"><br>{{jtIntro}}</p><p><a class="btn btn-lg btn-success" ng-href="#">{{jtButton}}</a></p></div>',
+      restrict: 'EA',
+			replace: true,
+			scope: {
+				jtHeading: '@',
+				jtIntro: '@',
+				jtButton: '@'
+			}			     
+    };
+  });

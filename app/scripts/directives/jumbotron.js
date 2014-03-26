073934b5ca15e6ae688d002b0281c2faa48d3154
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-03-26 10:15:38 +0000

    Putting jumbotrons to better use

--- app/scripts/directives/jumbotron.js
+++ app/scripts/directives/jumbotron.js
@@ -3,7 +3,7 @@
 angular.module('farnboroughyoApp')
   .directive('jumbotron', function () {
     return {
-			template: '<div class="jumbotron"><h1>{{jtHeading}}</h1><p class="lead"><img src="images/yeoman.png" alt="Im Yeoman"><br>{{jtIntro}}</p><p><a class="btn btn-lg btn-success" ng-href="#">{{jtButton}}</a></p></div>',
+			template: '<div class="jumbotron"><h1>{{jtHeading}}</h1><p class="lead">{{jtIntro}}</p><p><a class="btn btn-lg btn-success" ng-href="#">{{jtButton}}</a></p></div>',
       restrict: 'EA',
 			replace: true,
 			scope: {

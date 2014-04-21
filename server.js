0bfd33b25d95dd425ca7b7b764b2ec9876184f8b
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-04-21 20:34:28 +0000

    Changed the server script

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-'use strict';
-
-/**
- * Module dependencies.
- */
-
-var express = require('express');
-
-// NL - Changed this to 3000 because I can't run it via 8080 on my nitrous.io dev environment
-var config = {
-    port: 3000
-};
-
-var app = express();
-
-app.use(express.static(__dirname + '/app'));
-app.listen(config.port);
-
-console.log('Express app started on port ' + config.port);
+var app = require('express')();
+app.use(express.static('app'));
+app.listen(process.env.PORT);

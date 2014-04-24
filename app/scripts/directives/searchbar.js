79560961adf9f83849218abff8502fff0c024b2a
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-04-24 23:00:34 +0000

    Done stuff

--- app/scripts/directives/searchbar.js
+++ app/scripts/directives/searchbar.js
@@ -3,7 +3,7 @@
 angular.module('farnboroughyoApp')
   .directive('searchbar', function () {
     return {
-      template: '<div class="col-md-12 card"><div class="status-bar"><input type="text" ng-model="search" class="form-control" placeholder="Search"></div></div>',
+      template: '<div class="col-md-12 card search"><div class="status-bar"><input type="text" ng-model="search" class="form-control" placeholder="Search"></div></div>',
       restrict: 'EA',
       replace: true
     };

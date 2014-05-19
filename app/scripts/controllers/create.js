8fd916483a98bad7b9037e25aee8046b514dc644
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-05-19 09:44:17 +0000

    Firstly we set our main fields and if images are added then we update the record

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,39 +1,41 @@
 'use strict';
 
 angular.module('farnboroughyoApp')
-	.controller('CreateCtrl', function ($scope, $location, $timeout, fbRequestUrl, fbURL) {
+	.controller('CreateCtrl', function ($scope, $location, $timeout, fbRequestUrl, fbURL, $anchorScroll) {
+        
   		$scope.save = function() {
-        var messageListRef = new Firebase(fbURL);
-        var newMessageRef = messageListRef.push();
-        //newMessageRef.set({'user_id': 'fred', 'text': 'Yabba Dabba Doo!'});
-        
-    	  $scope.place.updated = (new Date()).getTime();
-			  $scope.place.imageData = 0;
-	  	  var f = $scope.files[0];
-	  	  var reader = new FileReader();
-	  
-	  	  reader.onload = (function(theFile) {
-			    return function(e) {
-            var filePayload = e.target.result;
-				
-				    // Note that this will take time to upload, so have to fill some details in later
-				    // So using the spinner may be a good idea
-			 	    //var f = new Firebase(fbURL + 'pano' + '/filePayload');
-            newMessageRef.set({'name': $scope.place.name, 'description': $scope.place.description, 'file': filePayload});
-      			//newMessageRef.set(filePayload, function() {         			
-        			// Once uploaded, anything you's like to do, do it here        
-      			//});
-    		}; 
-	  	})(f);
-	  	reader.readAsDataURL(f);
-			
-      
-        
-    	//var newitem = fbRequestUrl.$add($scope.place, function() {
-      //		$timeout(function() { $location.path('/'); });
-    	//});
-        
-    	$location.path('/');
-  	};
+        if ($scope.myForm.$valid) {
+          var messageListRef = new Firebase(fbURL);
+          var newMessageRef = messageListRef.push();          
+
+          $scope.place.updated = (new Date()).getTime();
+          
+          //$scope.place.imageData = 0;
+          
+          newMessageRef.set({'name': $scope.place.name, 
+                             'description': $scope.place.description, 
+                             'updated': $scope.place.updated});
+          
+          if($scope.files) {
+            var f = $scope.files[0];
+            var reader = new FileReader();
+
+            reader.onload = (function(theFile) {
+              return function(e) {
+                var filePayload = e.target.result;
+                newMessageRef.update({'file': filePayload});
+              }; 
+            })(f);
+            reader.readAsDataURL(f);
+
+            
+          }
+          $location.path('/');
+        } else {
+          alert("Form invalid");
+          $location.hash('name');
+          $anchorScroll();
+        }
+      };
 });
 

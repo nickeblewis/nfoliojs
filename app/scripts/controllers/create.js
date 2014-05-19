2b29fd3e0a57ade237cce7f1c4b11070709298c2
nickeblewis@gmail.com <nickeblewis@gmail.com>
2014-05-19 13:31:39 +0000

    Added google maps funcationality

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -2,8 +2,17 @@
 
 angular.module('farnboroughyoApp')
 	.controller('CreateCtrl', function ($scope, $location, $timeout, fbRequestUrl, fbURL, $anchorScroll) {
-        
+     navigator.geolocation.getCurrentPosition(
+        function(position) {
+          $scope.place.lat = position.coords.latitude;
+          $scope.place.lng = position.coords.longitude;
+        });
+    
+    
   		$scope.save = function() {
+        
+        
+        
         if ($scope.myForm.$valid) {
           var messageListRef = new Firebase(fbURL);
           var newMessageRef = messageListRef.push();          
@@ -14,6 +23,8 @@ angular.module('farnboroughyoApp')
           
           newMessageRef.set({'name': $scope.place.name, 
                              'description': $scope.place.description, 
+                             'lat': $scope.lat,
+                             'lng': $scope.lng,
                              'updated': $scope.place.updated});
           
           if($scope.files) {
@@ -32,7 +43,6 @@ angular.module('farnboroughyoApp')
           }
           $location.path('/');
         } else {
-          alert("Form invalid");
           $location.hash('name');
           $anchorScroll();
         }

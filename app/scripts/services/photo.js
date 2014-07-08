1efa7b56c9f0eabef5bcc1d8f5795327b6517f33
lewisn2 <Nicholas.Lewis@TheAA.com>
2014-07-08 15:47:13 +0100

    Got scaling sussed for top image and the thumbnails below it.

--- app/scripts/services/photo.js
+++ app/scripts/services/photo.js
@@ -24,7 +24,7 @@ angular.module('nfolio')
                    reader.onload = function(e) {
                      var img = new Image();
                      img.onload=function() {
-                       resizeUpload(this,700, 'medium', userFolder + '/' + imageFolder + '/medium/' + f.name, ref);
+                       resizeUpload(this,1080, 'medium', userFolder + '/' + imageFolder + '/medium/' + f.name, ref);
                      };
                      var img2 = new Image();
                      img2.onload=function(){
